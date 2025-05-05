2ea442bc7e71fa17ba6c4b2505f4622f3ab3cd78
Leves <levente@bbco.hu>
2025-05-05 12:55:07 +0200

    added incoming data model

---
+++ BACKEND/MonopolyManager/Models/IncomingData/GameCreationData.cs
@@ -0,0 +1,10 @@
+﻿namespace MonopolyManager.Models.IncomingData
+{
+    public class GameCreationData
+    {
+        public int StartMoney { get; set; }
+        public int StartTileMoney {  get; set; }
+        public string Owner { get; set; }
+        public List<string> Players { get; set; }
+    }
+}
